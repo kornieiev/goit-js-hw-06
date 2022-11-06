e2854ebc46dd012ebd8ad67dd086eb60bf0cf3d6
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2022-11-06 17:52:00 +0200

    add some practice

--- js/task-02.js
+++ js/task-02.js
@@ -11,8 +11,6 @@
 у список ul#ingredients.
 */
 
-
-
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -24,11 +22,20 @@ const ingredients = [
 
 const ul = document.querySelector("#ingredients");
 // const li = ingredients.length;
+console.log('with for method:')
 for (let i = 0; i < ingredients.length; i++) {
   const liIngredients = document.createElement('li');
   liIngredients.textContent = ingredients[i];
   liIngredients.class = 'item';
   ul.append(liIngredients);
   console.log(ul);
-  console.log(liIngredients.class);
 }
+console.log('')
+console.log('with forEach method:')
+ingredients.forEach( function (ingredient, index) {
+  const liIngredients = document.createElement('li');
+  liIngredients.textContent = ingredients[index];
+  liIngredients.class = 'item';
+  ul.append(liIngredients);
+  console.log(ul);
+})
